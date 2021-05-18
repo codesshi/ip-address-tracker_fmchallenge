2b5d4c5b37fd1bedfac648aa4059bdc4e0d65d79
Fabrizio Giannobili <codesshi@gmail.com>
2021-05-18 16:23:13 +0200

    [FIX] Avoid creation of a new map instance on every re-render

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,25 +1,39 @@
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import L from 'leaflet';
+
+const useMap = (lat, lng, id = "map") => {
+    const mapRef = useRef()
+
+    useEffect(() => {
+        if (!mapRef.current) {
+            const map = L.map('map', {zoomControl: false});
+            const customIcon = L.icon(
+                {
+                    iconUrl: "icon-location.svg",
+                    iconAnchor: [23, 56],
+                });
+            const marker = L.marker([lat, lng], {icon: customIcon}).addTo(map);
+            L.tileLayer("https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?{token}", {token: "key=v9ZdPTmc692sQ8sJaa1F"}).addTo(map);
+
+            mapRef.current = {map, marker}
+        }
+
+        mapRef.current.map.setView([lat, lng], 16);
+        mapRef.current.marker.setLatLng([lat, lng]);
+    });
+
+    useEffect(() => {
+        return () => {
+            mapRef.current.map.remove();
+            mapRef.current = undefined;
+        }
+    }, []);
+}
 
 const Map = (props) => {
     const {lat, lng} = props.coord;
 
-    console.log('rendered');
-
-    useEffect(() => {
-        console.log('useEffect');
-        const map = L.map('map', {zoomControl: false}).setView([lat, lng], 16);
-        const customIcon = L.icon(
-            {
-                iconUrl: "icon-location.svg",
-                iconAnchor: [23, 56],
-            });
-
-        L.marker([lat, lng], {icon: customIcon}).addTo(map);
-        L.tileLayer("https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=v9ZdPTmc692sQ8sJaa1F").addTo(map);
-
-        return () => {map.remove()};
-    }, [lat, lng]);
+    useMap(lat, lng);
 
     return (
         <div id="map" style={{height: "100%"}}></div>
