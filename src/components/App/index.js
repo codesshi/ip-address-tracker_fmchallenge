4618e4fda9b2e5eb33617847f6acd199e315fb22
Fabrizio Giannobili <codesshi@gmail.com>
2021-06-06 19:48:18 +0200

    [FEAT] Show status of search request

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,22 +2,13 @@ import './App.css';
 import SearchBar from '../SearchBar';
 import IpInfoCollapsible from '../IpInfoCollapsible';
 import Map from '../Map';
-import {useState, useEffect} from 'react';
+import {FetchingSnackbar, ErrorSnackbar} from '../Snackbar';
+import {useState, useEffect, useRef} from 'react';
+import {isIPv4, isIPv6} from '../../utils/IPValidation';
+import isDomain from '../../utils/DomainValidation';
 
-function App() {
-  const endpoint = process.env.REACT_APP_IPINFO_ENDPOINT_TEST;
-  const key = process.env.REACT_APP_IPINFO_KEY;
-  const [info, setInfo] = useState(
-    {
-      ip: "192.212.174.101",
-      location: "Brooklyn, NY 10001",
-      timezone: "UTC -05:00",
-      isp: "SpaceX Starlink",
-      coord: {lat: 0, lng: 0}
-    }
-  );
-
-  // set fixed height to avoid viewport resize when displaying virtual keyboard
+// set fixed height to avoid viewport resize when displaying virtual keyboard
+function useVirtualKeyboardFix() {
   const [height, setHeight] = useState("100%");
 
   useEffect(() => {
@@ -25,12 +16,56 @@ function App() {
     setHeight(dimesions.height + "px");
   }, []);
 
-  const handleSearch = (str) => {
-    fetch(`${endpoint}?apiKey=${key}&ipAddress=${str}`)
-      .then(response => response.json())
+  return height;
+}
+
+function useIpify() {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState();
+  const [isFetching, setIsFetching] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorType, setErrorType] = useState("");
+  const lastQueryRef = useRef({stale: false});
+
+  useEffect(() => {
+    const endpoint = process.env.REACT_APP_IPINFO_ENDPOINT;
+    const key = process.env.REACT_APP_IPINFO_KEY;
+    const currentQuery = {stale: false};
+    lastQueryRef.current.stale = true;
+    lastQueryRef.current = currentQuery;
+
+    let search;
+
+    if (query === "")
+      search = "ipAddress=";
+    if (isIPv4(query) || isIPv6(query))
+      search = `ipAddress=${query}`;
+    if (isDomain(query))
+      search = `domain=${query}`;
+    if (!search) {
+      setIsError(true);
+      setErrorType("Invalid Input");
+      return;
+    }
+
+    setIsFetching(true);
+    setIsError(false);
+
+    fetch(`${endpoint}?apiKey=${key}&${search}`)
+      .then(response => {
+        if (!response.ok) {
+          throw response.statusText;
+        }
+        
+        return response.json();
+      })
       .then(data => {
-        console.log(data);
-        setInfo(
+        if (currentQuery.stale) {
+          return;
+        }
+        
+        setIsFetching(false);
+        setResult(
           {
             ip: data.ip,
             location: data.location.city,
@@ -39,7 +74,31 @@ function App() {
             coord: {lat: data.location.lat, lng: data.location.lng}
           }
         );
+      })
+      .catch(err => {
+        if (currentQuery.stale)
+          return;
+        
+        const errorMessage = err.message === "Failed to fetch" ? "Connection Unavailable" : "Data Not Found";
+        setIsError(true);
+        setErrorType(errorMessage);
+        setIsFetching(false);
+        setResult();
       });
+  }, [query]);
+
+  return [{result, isFetching, isError, errorType}, setQuery];
+}
+
+function App() {
+  const height = useVirtualKeyboardFix();
+  const [{result, isFetching, isError, errorType}, search] = useIpify();
+
+  const handleSearch = (str) => {
+    if (str === "")
+      return;
+
+    search(str);
   }
 
   return (
@@ -47,13 +106,15 @@ function App() {
       <header>
         <h1>IP Address Tracker</h1>
         <SearchBar onSearch={handleSearch} />
-        <div className="resultContainer">
-          <IpInfoCollapsible info={info}/>
+        <div className="result-container">
+          {result && <IpInfoCollapsible info={result}/>}
         </div>
       </header>
       <div className="map-container">
-        <Map coord={info.coord}/>
+        <Map coord={result && result.coord}/>
       </div>
+      {<ErrorSnackbar show={isError}>{errorType}</ErrorSnackbar>}
+      {<FetchingSnackbar show={isFetching} />}
     </div>
   );
 }
